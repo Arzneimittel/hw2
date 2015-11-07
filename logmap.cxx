2921a5a35b647ef4c99678686b337fe4612a1df8
Patrick Laermann <patrick@linux-4xun.site>
2015-11-07 13:10:40 +0100

    Änderung

--- logmap.cxx
+++ logmap.cxx
@@ -1,23 +1,29 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main(){
 	double x0 = 0.5;
-	double x;
+	double k;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+	double x[(Nend-Nskip)];
+	ofstream out("output.txt");
 
 	for(double r=0; r <= 4; r += 0.001){
-	   x=x0;
+	   k=x0;
 	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
+		   k = r*k*(1-k);
 	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
+	   		   x[i-(Nend-Nskip)-1] = r*k*(1-k);
+			   k=x[i-(Nend-Nskip)-1];
+	   		   //cout << r << "\t" << x[i-(Nend-Nskip)-1] << endl;
+   	       }
+   	       
+   	   for(int i=0; i<(Nend-Nskip); i++){
+	      out << r << "\t" << x[i] << endl;
+	       }  
 	}
-
-
+	out.close();
 	return 0;
 }
